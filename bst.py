c71541677452ebceba307bd3ae8a8cd93ebd6461
MaximumMaxx <maxwellwindland@gmail.com>
2026-02-18 09:43:59 -0800

    Setup project basics

---
+++ bst.py
@@ -0,0 +1,28 @@
+﻿import sys
+import unittest
+from typing import *
+from dataclasses import dataclass
+
+sys.setrecursionlimit(10 ** 9)
+
+# ---- Data types ----
+T = TypeVar("T")
+
+BinTree: TypeAlias = Optional["Node"[T]]
+
+
+@dataclass(frozen=True)
+class Node(Generic[T]):
+    value: Any
+    left: BinTree
+    right: BinTree
+
+
+@dataclass(frozen=True)
+class BinarySearchTree(Generic[T]):
+    tree: BinTree[T]
+    comes_before: Callable[[T, T], bool]
+
+
+# ---- Functions ----
+test: BinarySearchTree[int] = BinarySearchTree(Node(10, None, None), lambda x, y: x < y)
